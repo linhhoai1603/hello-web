d2e53e60079342dc6ac50da171779d3f7dd00a59
Huynh Linh Hoai <22130084@st.hcmuaf.edu.vn>
2026-03-12 11:04:03 +0700

    fix in branch hoai

--- src/main/java/com/badminton/shop/helloweb/HelloController.java
+++ src/main/java/com/badminton/shop/helloweb/HelloController.java
@@ -12,12 +12,12 @@ public class HelloController {
 
     @GetMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> root() {
-        return ResponseEntity.ok("This is my website to test CI/CD Pipeline");
+        return ResponseEntity.ok("This is my website to test CI/CD Pipeline, and welcome to my website");
     }
 
     @GetMapping(path = "/api/hello", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, String>> apiHello() {
-        return ResponseEntity.ok(Map.of("message", "Hello my friends, my name is Hoài"));
+        return ResponseEntity.ok(Map.of("message", "Hello my friends, my name is Hoài. This is change by Hoài"));
     }
 }
 
